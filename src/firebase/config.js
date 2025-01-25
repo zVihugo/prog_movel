f2df7ba6654c6e05c117c551aebf4aa3079ac6f9
drezix <andre.t.agostinis@gmail.com>
2025-01-25 16:16:57 -0300

    refactor(firebase): centralizar configuração e atualizar dependências

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
-import { initializeFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
+import { initializeFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyAfq0v5ONPCFbfQuuPPE382NsAID3i8bfI',
